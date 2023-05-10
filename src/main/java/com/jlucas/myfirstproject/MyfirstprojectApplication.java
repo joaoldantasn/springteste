6ff4b1bf456f1bfcf156b8fdc204dba82aad0429
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-10 18:56:39 -0300

    Category memory

---
+++ src/main/java/com/jlucas/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,30 @@
+package com.jlucas.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jlucas.myfirstproject.entities.Category;
+import com.jlucas.myfirstproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+	}
+
+}
